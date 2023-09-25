c6c2a6b0b5dd43b35e1cc6f951ae1dc0ed5b5524
FantasticFox91 <44309782+FantasticFox91@users.noreply.github.com>
2023-09-25 17:06:48 +0300

    update cors

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const video = require('./routes/video.js')
 const PORT = process.env.PORT || 5000;
 
 const corsOptions = {
-  origin: 'http://localhost:3000',
+  origin: 'https://sedona-nuxt.vercel.app/',
   methods: 'GET,POST',
   credentials: true,
 }
